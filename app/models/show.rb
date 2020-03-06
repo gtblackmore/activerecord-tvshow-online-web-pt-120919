b39513295d940bd8cf91e468b93be02a237624e7
George Blackmore <gtblackmore@gmail.com>
2020-03-06 16:53:52 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,26 +3,28 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end
   
-  def most_popular_show
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
     
   end
   
-  def lowest_rating
-    Show.minimum(:rating)
+  def self.lowest_rating
+    self.minimum(:rating)
   end
   
-  def least_popular_show
+  def self.least_popular_show
+    self.where("rating = >", self.lowest_rating).first
   end
   
-  def ratings_sum
-    Show.sum(:rating)
+  def self.ratings_sum
+    self.sum(:rating)
   end
   
-  def popular_shows
-    Show.where("rating > 5")
+  def self.popular_shows
+    self.where("rating > 5")
   end
   
-  def shows_by_alphabetical_order
-    Show.all.order(:name)
+  def self.shows_by_alphabetical_order
+    self.all.order(:name)
   end
 end
