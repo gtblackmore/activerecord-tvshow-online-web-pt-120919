b36950477c298c758eda6792e01dd269772ec4d3
George Blackmore <gtblackmore@gmail.com>
2020-03-06 16:49:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,20 +4,25 @@ class Show < ActiveRecord::Base
   end
   
   def most_popular_show
+    
   end
   
   def lowest_rating
+    Show.minimum(:rating)
   end
   
   def least_popular_show
   end
   
   def ratings_sum
+    Show.sum(:rating)
   end
   
   def popular_shows
+    Show.where("rating > 5")
   end
   
   def shows_by_alphabetical_order
+    Show
   end
 end
